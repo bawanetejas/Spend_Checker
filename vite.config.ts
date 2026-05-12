4eef778c112982dda3125cca487143ec04959e2e
bawanetejas <tejasbawane221@gmail.com>
2026-05-12 09:13:56 +0530

    chore: setup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression2';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    compression({
+      algorithms: ['gzip'],
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+
+    compression({
+      algorithms: ['brotliCompress'],
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+  ],
+  build: {
+    target: 'es2015',
+
+    minify: 'terser',
+
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react')) {
+            return 'react-vendor';
+          }
+        },
+      },
+    },
+
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false,
+  },
+
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+});
